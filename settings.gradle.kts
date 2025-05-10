6644c6cd088c4793de98d320bf7850902630e765
Greem4 <i@greem.ru>
2025-05-10 19:44:47 +0300

    мини ап build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,7 @@
 pluginManagement {
     plugins {
+        kotlin("plugin.spring") version "2.1.20"
+        kotlin("plugin.jpa")    version "2.1.20"
         kotlin("jvm") version "2.1.20"
     }
 }
